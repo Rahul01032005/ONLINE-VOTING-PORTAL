d75c6da475591c8c134320a7b6dbfa29091163dd
Rahul E <149226615+Rahul01032005@users.noreply.github.com>
2025-02-17 18:43:49 +0530

    Add files via upload

---
+++ voted_list.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+namespace Election
+{
+    #region Voted_list
+    public class Voted_list
+    {
+        #region Member Variables
+        protected string _voted;
+        #endregion
+        #region Constructors
+        public Voted_list() { }
+        public Voted_list(string voted)
+        {
+            this._voted=voted;
+        }
+        #endregion
+        #region Public Properties
+        public virtual string Voted
+        {
+            get {return _voted;}
+            set {_voted=value;}
+        }
+        #endregion
+    }
+    #endregion
+}
---
+++ voters.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+namespace Election
+{
+    #region Voters
+    public class Voters
+    {
+        #region Member Variables
+        protected string _username;
+        protected string _regno;
+        #endregion
+        #region Constructors
+        public Voters() { }
+        public Voters(string username)
+        {
+            this._username=username;
+        }
+        #endregion
+        #region Public Properties
+        public virtual string Username
+        {
+            get {return _username;}
+            set {_username=value;}
+        }
+        public virtual string Regno
+        {
+            get {return _regno;}
+            set {_regno=value;}
+        }
+        #endregion
+    }
+    #endregion
+}
